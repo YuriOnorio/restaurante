3bd4ef283600b0f0bcd252fbfacea5d9f172f489
YuriOnorio <yronorio@gmail.com>
2023-08-21 12:22:28 -0300

    .

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,19 @@
-import styles from './Cardapio.module.scss'
-import logo from '../../assets/logo.svg'
+import styles from './Cardapio.module.scss';
+import logo from 'assets/logo.svg'
 
-export default function Cardapio() {
-    return(
+export default function Cardapio(){
+    return (
         <main>
             <nav className={styles.menu}>
-                <img src={logo} alt="logo" />
+                <img src={ logo } alt="logo" />
             </nav>
+
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa !!!
+                </div>
+            </header>
+
         </main>
-    )
+    );
 }
