4d8af817f29d3a69151b5f8ff69c6b1fb0dc3a79
YuriOnorio <yronorio@gmail.com>
2023-10-05 11:47:01 -0300

    .

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,21 +1,33 @@
-import styles from './Filtros.module.scss';
-import filtros from './filtros.json';
+import styles from "./Filtros.module.scss";
+import filtros from "./filtros.json";
 
-type IOpcao = typeof filtros[0];
+type IOpcao = (typeof filtros)[0];
 
-/*interface IOpcao {
-    id: number;
-    label: string;
-}*/
+interface Props {
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
 
-export default function Filtros() {
-    return(
-        <div className={styles.filtro}>
-            {filtros.map((opcao) => (
-                <button className={styles.filtro__filtro} key={opcao.id}>
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
-    )
+export default function Filtros({ filtro, setFiltro }: Props) {
+  function selecionarFiltro(opcao: IOpcao) {
+    return setFiltro(opcao.id);
+  }
+  return (
+    <div className={styles.filtro}>
+      {filtros.map((opcao) => (
+        <button
+          className={`
+
+          ${styles.filtro__filtro}
+          ${filtro === opcao.id ? styles["filtro__filtro--ativo"] : ""}
+
+          `}
+          key={opcao.id}
+          onClick={() => selecionarFiltro(opcao)}
+        >
+            {opcao.label}
+        </button>
+      ))}
+    </div>
+  );
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -6,13 +6,14 @@ import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState <number | null> (null);
   return (
     <main>
       <nav className={styles.menu}>
         <img src={logo} alt="logo" />
       </nav>
 
-      <header className={styles.header}>
+     <header className={styles.header}>
         <div className={styles.header__text}>
           A casa do código e da massa !!!
         </div>
@@ -22,9 +23,9 @@ export default function Cardapio() {
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
-        <Filtros />
+        <Filtros filtro={filtro} setFiltro={setFiltro} />
       </div>
-      </section>
-    </main>
+      </section>  
+  </main> 
   );
 }
